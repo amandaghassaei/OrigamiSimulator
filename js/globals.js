8ed24ea85d1bd2770588f88f58d83fd371f99132
amandaghassaei <amandaghassaei@gmail.com>
2017-03-27 04:57:28 -0400

    small changes

--- js/globals.js
+++ js/globals.js
@@ -25,7 +25,7 @@ function initGlobals(){
         //sim settings
         creasePercent: 0,
         axialStiffness: 100,
-        creaseStiffness: 1,
+        creaseStiffness: 30,
         panelStiffness: 1,
 
         //dynamic sim settings
