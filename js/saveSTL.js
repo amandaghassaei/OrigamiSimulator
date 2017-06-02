aad83d50d93488c1151a9453d91a5cc7f1669f0c
amandaghassaei <amandaghassaei@gmail.com>
2017-06-02 16:24:46 -0400

    simple fold examples

--- js/saveSTL.js
+++ js/saveSTL.js
@@ -15,71 +15,69 @@ function makeSaveGEO(doublesided){
     }
 
 
-    if (globals.thickenModel){
-        var numVertices = geo.vertices.length;
-        geo.computeVertexNormals();
-        geo.computeFaceNormals();
-        for (var i=0;i<numVertices;i++){
-            var face;
-            var vertexNormal = new THREE.Vector3();
-            var lastFaceIndex = 0;
-            for (var j=0;j<geo.faces.length;j++){
-                face = geo.faces[j];
-                if (face.a == i) {
-                    var a = geo.vertices[face.a];
-                    var b = geo.vertices[face.b];
-                    var c = geo.vertices[face.c];
-                    var  weight = Math.abs(Math.acos( (b.clone().sub(a)).normalize().dot( (c.clone().sub(a)).normalize() ) ));
-                    console.log(weight);
-                    vertexNormal.add(face.normal.clone().multiplyScalar(weight));
-                    lastFaceIndex = j;
-                } else if (face.b == i) {
-                    var a = geo.vertices[face.a];
-                    var b = geo.vertices[face.b];
-                    var c = geo.vertices[face.c];
-                    var  weight = Math.abs(Math.acos( (c.clone().sub(b)).normalize().dot( (a.clone().sub(b)).normalize() ) ));
-                    console.log(weight);
-                    vertexNormal.add(face.normal.clone().multiplyScalar(weight));
-                    lastFaceIndex = j;
-                } else if (face.c == i) {
-                    var a = geo.vertices[face.a];
-                    var b = geo.vertices[face.b];
-                    var c = geo.vertices[face.c];
-                    var  weight = Math.abs(Math.acos( (b.clone().sub(c)).normalize().dot( (a.clone().sub(c)).normalize() ) ));
-                    console.log(weight);
-                    vertexNormal.add(face.normal.clone().multiplyScalar(weight));
-                    lastFaceIndex = j;
-                }
-                // if (vertexNormal !== null) break;
-            }
-            // if (vertexNormal === undefined) {
-            //     geo.vertices.push(new THREE.Vector3());
-            //     continue;
-            // }
-            //filter out duplicate normals
-            vertexNormal.normalize();
-            var offset = vertexNormal.clone().multiplyScalar(globals.thickenOffset/(2*vertexNormal.clone().dot(geo.faces[lastFaceIndex].normal)));
-
-            // geo.vertices.push(geo.vertices[i].clone().sub(offset));
-            geo.vertices[i].add(offset);
-        }
-        // var numFaces = geo.faces.length;
-        // for (var i=0;i<numFaces;i++){
-        //     var face = geo.faces[i].clone();
-        //     face.a += numVertices;
-        //     face.b += numVertices;
-        //     face.c += numVertices;
-        //     var b = face.b;
-        //     face.b = face.c;
-        //     face.c = b;
-        //     geo.faces.push(face);
-        // }
-        geo.computeVertexNormals();
-        geo.computeFaceNormals();
+    // if (globals.thickenModel){
+    //     var numVertices = geo.vertices.length;
+    //     geo.computeVertexNormals();
+    //     geo.computeFaceNormals();
+    //     for (var i=0;i<numVertices;i++){
+    //         var face;
+    //         var vertexNormal = new THREE.Vector3();
+    //         var lastFaceIndex = 0;
+    //         for (var j=0;j<geo.faces.length;j++){
+    //             face = geo.faces[j];
+    //             if (face.a == i) {
+    //                 var a = geo.vertices[face.a];
+    //                 var b = geo.vertices[face.b];
+    //                 var c = geo.vertices[face.c];
+    //                 var  weight = Math.abs(Math.acos( (b.clone().sub(a)).normalize().dot( (c.clone().sub(a)).normalize() ) ));
+    //                 vertexNormal.add(face.normal.clone().multiplyScalar(weight));
+    //                 lastFaceIndex = j;
+    //             } else if (face.b == i) {
+    //                 var a = geo.vertices[face.a];
+    //                 var b = geo.vertices[face.b];
+    //                 var c = geo.vertices[face.c];
+    //                 var  weight = Math.abs(Math.acos( (c.clone().sub(b)).normalize().dot( (a.clone().sub(b)).normalize() ) ));
+    //                 vertexNormal.add(face.normal.clone().multiplyScalar(weight));
+    //                 lastFaceIndex = j;
+    //             } else if (face.c == i) {
+    //                 var a = geo.vertices[face.a];
+    //                 var b = geo.vertices[face.b];
+    //                 var c = geo.vertices[face.c];
+    //                 var  weight = Math.abs(Math.acos( (b.clone().sub(c)).normalize().dot( (a.clone().sub(c)).normalize() ) ));
+    //                 vertexNormal.add(face.normal.clone().multiplyScalar(weight));
+    //                 lastFaceIndex = j;
+    //             }
+    //             // if (vertexNormal !== null) break;
+    //         }
+    //         // if (vertexNormal === undefined) {
+    //         //     geo.vertices.push(new THREE.Vector3());
+    //         //     continue;
+    //         // }
+    //         //filter out duplicate normals
+    //         vertexNormal.normalize();
+    //         console.log(vertexNormal);
+    //         var offset = vertexNormal.clone().multiplyScalar(5);//globals.thickenOffset/(2*vertexNormal.clone().dot(geo.faces[lastFaceIndex].normal)));
+    //
+    //         geo.vertices.push(geo.vertices[i].clone().sub(offset));
+    //         geo.vertices[i].add(offset);
+    //     }
+    //     var numFaces = geo.faces.length;
+    //     for (var i=0;i<numFaces;i++){
+    //         var face = geo.faces[i].clone();
+    //         face.a += numVertices;
+    //         face.b += numVertices;
+    //         face.c += numVertices;
+    //         var b = face.b;
+    //         face.b = face.c;
+    //         face.c = b;
+    //         geo.faces.push(face);
+    //     }
+    //     geo.computeVertexNormals();
+    //     geo.computeFaceNormals();
 
 
 
-    } else if (doublesided){
+    if (doublesided){
         var numFaces = geo.faces.length;
         for (var i=0;i<numFaces;i++){
             var face = geo.faces[i];
