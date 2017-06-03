7a231bb5c4fdedcf5a0d8f01d3d28b33e15e40c7
amandaghassaei <amandaghassaei@gmail.com>
2017-06-03 14:51:21 -0400

    small changes

--- js/pattern.js
+++ js/pattern.js
@@ -623,8 +623,9 @@ function initPattern(globals){
         }
         if (_weededVertices.length > 0){
             // console.log(_weededVertices);
-            if (badColors.length==0) globals.warn("Some vertices are not fully connected, try increasing vertex merge tolerance.<br/>" +
-                "Aborting file import.");
+            if (badColors.length==0) globals.warn("<br/>Some vertices are not fully connected,<br/>" +
+                "try increasing vertex merge tolerance.<br/>" +
+                "Aborting file import.<br/><br/>");
             else $("#warningMessage").append("This error caused the file import to abort.<br/><br/>");
             return true;
         }
