e1c954108c66844115ad9b579babe28094c291cf
amandaghassaei <amandaghassaei@gmail.com>
2018-03-26 09:13:30 -0700

    ui fixes

--- js/globals.js
+++ js/globals.js
@@ -81,7 +81,7 @@ function initGlobals(){
         userInteractionEnabled: false,
         vrEnabled: false,
 
-        numSteps: 10,
+        numSteps: 100,
 
         rotateModel: null,
         rotationSpeed: 0.01,
