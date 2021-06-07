1d421ba7b066abc5f2e3c5169021d7549f83e29e
jtpelster <76790269+jtpelster@users.noreply.github.com>
2021-06-07 14:39:47 -0400

    Rm-Matlab

--- MatlabPlotter/accuracyplot.m
+++
@@ -1,8 +0,0 @@
-plot(accuracy(:,1),accuracy(:,2))
-hold on
-plot(accuracy(:,1),accuracy(:,3))
-plot(accuracy(:,1),accuracy(:,4))
-plot(accuracy(:,1),accuracy(:,5))
-legend('crease 1','crease 2', 'crease 3', 'crease 4')
-hold off
-save accuracy
